1eecb84ad0369c7c5d88e81fbd3a8f0dda6edf86
Partyson <bogdan.shustik1@mail.ru>
2023-04-28 01:01:29 +0500

    разбираемся с комитами

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,15 +15,36 @@ namespace Game
     {
         public Form1()
         {
+            var label = new Label();
+            label.Location = new Point(0, 0);
+            label.Size = new Size(ClientSize.Width, 30);
+            label.Text = "Input number";
+            Controls.Add(label);
+            var input = new TextBox();
+            input.Location = new Point(0, label.Bottom);
+            input.Size = label.Size;
+            Controls.Add(input);
+
+            var button = new Button();
+            button.Location = new Point(0, input.Bottom);
+            button.Size = label.Size;
+            button.Text = "Increment";
+            button.Click += (sender, args) =>
+            {
+                var number = int.Parse(input.Text);
+                number++;
+                input.Text = number.ToString();
+            };
+            Controls.Add(button);
             InitializeComponent();
         }
         
         protected override void OnFormClosing(FormClosingEventArgs eventArgs)
-                {
-                    var result = MessageBox.Show("Действительно закрыть?", "",
-                        MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                    if (result != DialogResult.Yes)
-                        eventArgs.Cancel = true;
-                }
+        {
+            var result = MessageBox.Show("Действительно закрыть?", "",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes) 
+                eventArgs.Cancel = true;
+        }
     }
 }
--- Game/Program.cs
+++ Game/Program.cs
@@ -17,7 +17,6 @@ namespace Game
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Console.WriteLine("aAboba");
         }
     }
 }
