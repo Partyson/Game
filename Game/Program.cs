1e81d6e8e86f43f0f2415d93748dd300cbef4dce
Partyson <bogdan.shustik1@mail.ru>
2023-04-27 17:44:08 +0500

    aboba

--- Game/Program.cs
+++ Game/Program.cs
@@ -17,6 +17,7 @@ namespace Game
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("aAboba");
         }
     }
 }
